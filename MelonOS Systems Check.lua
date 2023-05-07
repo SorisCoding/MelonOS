8edb6e64a19b80e66ccdfb4c051fb536e6a4e804
SorisCoding <132685796+SorisCoding@users.noreply.github.com>
2023-05-07 11:53:38 +0100

    MelonOS-DEV

--- MelonOS Systems Check.lua
+++ MelonOS Systems Check.lua
@@ -1,18 +1,18 @@
 shell.execute("clear")
 local SETUP = true
 local INSTALLED = false
-print("//BCOS Systems Check//")
-if fs.exists("/BCOS") then
+print("//MelonOS Systems Check//")
+if fs.exists("/MelonOS") then
     print("OS detected")
     local INSTALLED = true
 else
-    printError("OS not installed, installation required")
+    printError("MelonOS not installed, installation required")
     local INSTALLED = false
-    print("Would you like to install BCOS? (Y/N)")
+    print("Would you like to install MelonOS? (Y/N)")
     while true do
         local INSTALL = io.read()
         if INSTALL == "Y" then
-            print("Installing BCOS")
+            print("Installing MelonOS")
             shell.execute("clear")
             shell.execute("pastebin","run","FiThEB2N")
             break
@@ -24,12 +24,12 @@ else
         end
     end
 end
-if fs.exists("/BCOS/usr") and INSTALLED then
+if fs.exists("/MelonOS/usr") and INSTALLED then
     print("User files detected, SETUP set to false")
     local SETUP = false
     print("")
-    print("Booting BCOS")
-    shell.run("/BCOS/BCOS.lua")
+    print("Booting MelonOS")
+    shell.run("/MelonOS/MelonOS.lua")
 elseif SETUP and INSTALLED then
     printError("User files not detected, SETUP set to true")
     local SETUP = true
