4109b5ace6710cff71b6701eb90c7075bf2093f7
SorisCoding <132685796+SorisCoding@users.noreply.github.com>
2023-05-16 14:10:49 +0100

    Update Installation-DEV.lua

--- Installation-DEV.lua
+++ Installation-DEV.lua
@@ -1,5 +1,11 @@
 print("Installation script in development")
 if not fs.exists("/MelonOS/functions/GitGet.lua") then
+	print("Downloading GitGet")
 	local gitgetcode = http.get("https://raw.githubusercontent.com/SorisCoding/MelonOS/MelonOS/MelonOS_Files/GitGet.lua")
-	local gitgetfile = fs.open("/MelonOS/System/GitGet.lua", "w")
+	local gitgetfile = fs.open("/MelonOS/function/GitGet.lua", "w")
+	gitgetfile.write(gitgetcode)
+	gitgetfile.close()
+	if fs.exists("/MelonOS/functions/GitGet.lua") then
+		print("GitGet Downloaded")
+	end
 end
