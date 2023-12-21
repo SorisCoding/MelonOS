38029d207f84916acb8a4d2803e5b9feae80f15b
SorisCode <132685796+SorisCoding@users.noreply.github.com>
2023-12-21 18:19:15 +0000

    Update Installation-DEV.lua

--- Installation-DEV.lua
+++ Installation-DEV.lua
@@ -5,23 +5,42 @@ print("  __  __      _              ___  ____
  	|_|  |_|\___|_|\___/|_| |_|\___/|____/ ")
 
 print("Installation script in development")
+local GitGetExists = False
 if not fs.exists("/MelonOS/functions/GitGet.lua") then
-	print("Downloading GitGet")
+	print("Not Found: GitGet")
+	local GitGetExists = False
+	
+	print("Downloading: GitGet")
 	local gitgetcode = http.get("https://raw.githubusercontent.com/SorisCoding/MelonOS/MelonOS/MelonOS_Files/GitGet-DEV.lua")
 	local gitgetfile = fs.open("/MelonOS/function/GitGet.lua", "w")
+	
 	gitgetfile.write(gitgetcode.readAll())
 	gitgetfile.close()
+	
 	if fs.exists("/MelonOS/function/GitGet.lua") then
-		print("GitGet Downloaded")
+		print("Downloaded: GitGet")
 	end
+	
 	local check = http.get("https://raw.githubusercontent.com/SorisCoding/MelonOS/MelonOS/MelonOS_Files/GitGet-DEV.lua")
 	local gitgetfile = fs.open("/MelonOS/function/GitGet.lua", "r")
+	
 	if not (gitgetfile.readAll() = check.readAll()) then
-		print("GitGet  A Update")
+		print("GitGet Might Need An Update")
+	end
+else
+	print("Found: GitGet")
+	local GitGetExists = True
 end
 
-if not (gitgetfile.readAll() = check.readAll()) then
-		print("GitGet  A Update")
+print("Checking for updates: GitGet")
+if not (gitgetfile.readAll() = check.readAll()) and GitGetExists then
+	print("GitGet Might Need An Update A Update")
+	print("Update script not made for: GitGet")
+end
 
+local OSUIExists = false
 if not fs.exists("/MelonOS/functions/OSUI.lua") then
-	print("Downloading OSUI")
+	print("Not Found: OSUI")
+	local OSUIExists = false
+	print("Download script not made for: OSUI")
+end
